887096407c6ddba69372894c41c3e0b1207fe0a9
Victor <victorlap@outlook.com>
2025-10-22 09:40:50 +0200

    Update constructor to allow nullable resolveCallback
    Fixes the following error:
    
    PHP Deprecated:  Ardenthq\ImageGalleryField\ImageGalleryField::__construct(): Implicitly marking parameter $resolveCallback as nullable is deprecated, the explicit nullable type must be used instead in /var/www/html/vendor/ardenthq/nova-image-gallery-field/src/ImageGalleryField.php on line 46

--- src/ImageGalleryField.php
+++ src/ImageGalleryField.php
@@ -43,7 +43,7 @@ class ImageGalleryField extends Trix
      * @param  (callable(mixed, mixed, ?string):mixed)|null  $resolveCallback
      * @return void
      */
-    public function __construct($name, $attribute = null, callable $resolveCallback = null)
+    public function __construct($name, $attribute = null, ?callable $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback);
 
